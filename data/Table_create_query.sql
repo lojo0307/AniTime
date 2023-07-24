ff8110c0a45e6f95b8053bd47f8764a9afb3ede5
권지훈 <aspire51@naver.com>
2023-07-24 12:58:03 +0900

    feat : member column update
    Former-commit-id: 0db7aab56bd330174c0c0d54a9d026efd863955e

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed4da37643445b02435994ffbdfe08af805c51f52f0cfd9633bed27157beb496
-size 8417
+oid sha256:2bc488cb4245e4e9d8515a013fdddfb997d05d31c35a4088eac1f7ef91c9dec5
+size 8453
