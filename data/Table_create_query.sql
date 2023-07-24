b206118ecc0f995deb17a87d309b7baf628129e5
권지훈 <aspire51@naver.com>
2023-07-24 10:29:57 +0900

    feat : code split

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa9a8cf6ecf9c76750e92d99fc90c26c7a0b82536c5b028639e710df99b441a6
-size 8307
+oid sha256:ed4da37643445b02435994ffbdfe08af805c51f52f0cfd9633bed27157beb496
+size 8417
