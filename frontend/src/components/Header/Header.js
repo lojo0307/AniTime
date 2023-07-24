0febe270e9b57a22b0c78567cbf6160628c5185d
임성원 <dnjsndnjs@gmail.com>
2023-07-24 16:32:08 +0900

    Feat: Header 네비 구성
    Former-commit-id: a0975c438b7c691790b5221cabe11fb12efd7044

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4761eb101e311067def3937545ee47794c6d071a0153d9e382449ceb4b2561aa
-size 634
+oid sha256:fb7594d3cc0ce4ed014039cce65e929c463e83622bc49e423e3297f8ab6e9b0c
+size 906
