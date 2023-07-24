719ead3e1b26b3d0291dafdb8c68ea010c6c1bc9
임성원 <dnjsndnjs@gmail.com>
2023-07-24 15:35:17 +0900

    Feat: Header 기본 구성
    Former-commit-id: 341acab941e5a100e9c857cc13f5eb3e2c106312

---
+++ frontend/src/components/Header/Logo.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8007decbb171905a800adb3d94c636e854e3dfcd62fb07886f9b283d95b7822b
+size 435
---
+++ frontend/src/components/Header/NavBar.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b59e0926b0f2c969aaf328c9fdc144e0bc40db74ba61c277aca76dc0ceabccb
+size 225
