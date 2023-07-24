acb0dac118576ded831850ab3392353193fa9b4b
권지훈 <aspire51@naver.com>
2023-07-24 13:01:42 +0900

    fix : query update
    Former-commit-id: c049df984cf59d97eb71b25f3b89305d3faa0cdd

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bc488cb4245e4e9d8515a013fdddfb997d05d31c35a4088eac1f7ef91c9dec5
-size 8453
+oid sha256:5e2605f26746eeeae22ec498a25535493b09d671fad265fd8789887d92b4eb61
+size 8452
