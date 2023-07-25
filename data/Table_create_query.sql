9c7d231ec52f1bc821a4dd5647b41e9c881900c7
권지훈 <aspire51@naver.com>
2023-07-25 14:41:41 +0900

    fix : env setup
    Former-commit-id: b5f003c4aa8d99c9418132409379500d9b78a556

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d49820cd4d9b00faa082d70cb0c8ba1ae0aee18eeb79e7d5f7434a6267b16dd7
-size 8477
+oid sha256:8dd56e574635f88de883302590a1bb23e9c492accfd55f6cf06764bcc42008ea
+size 8487
