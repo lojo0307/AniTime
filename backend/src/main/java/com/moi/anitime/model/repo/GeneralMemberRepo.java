adf67421a22ac173d640fbe42b47be72ceb02d8f
임성원 <dnjsndnjs@gmail.com>
2023-07-25 13:19:02 +0900

    Feat: 리액트 라우터 적용, 샘플 코드
    Former-commit-id: 84bcc62a6841969d4e6a36d0c5faa4eb2c958800

--- backend/src/main/java/com/moi/anitime/model/repo/GeneralMemberRepo.java
+++ backend/src/main/java/com/moi/anitime/model/repo/GeneralMemberRepo.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2475f8ecb9c28d5b2962b4f6cf91b4df34dbf2f36870442479499afbbd22ad09
-size 441
+package com.moi.anitime.model.repo;
+
+import com.moi.anitime.model.entity.member.GeneralMember;
+import com.moi.anitime.model.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GeneralMemberRepo extends JpaRepository<GeneralMember, Integer> {
+
+}
