b98a262096fa9df062e7268cee74b91f83e3393f
임성원 <dnjsndnjs@gmail.com>
2023-07-25 13:19:02 +0900

    Feat: 리액트 라우터 적용, 샘플 코드
    Former-commit-id: 24e0e0bf492eed2e02a0008d8f2b9329dbf23f15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a04ab1629f6437c19b17d925f8e26af40674040498b9d8d8ad240b0cd6a68386
-size 593
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
