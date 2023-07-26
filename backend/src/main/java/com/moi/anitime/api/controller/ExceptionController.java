1efb342410a1a32e26de3474b2bebd940ecad785
김민태 <kimmintae981019@gmail.com>
2023-07-26 16:25:03 +0900

    Feat: Stomp 소켓 설정, Chat 관련 Controller, Service, Repo 생성, 채팅방 목록 조회 API 완성
    Former-commit-id: a16c22cf549d071fb7c38cd5d75c3aeee3f54ba1

--- backend/src/main/java/com/moi/anitime/api/controller/ExceptionController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/ExceptionController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec247ad3debab6d9a7f7aded7442bc9d614c85d804be3236a70f6674426578b
-size 661
+oid sha256:dc363be9b6010a626546630c4938ef697e4b4a32890a8438ff12610f85dd798e
+size 703
---
+++ backend/src/main/java/com/moi/anitime/model/repo/ChatRoomRepo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cad0b2c856821f4fef80e266d76f622a6e9c97642a9a7e4680620b95af9b79c
+size 625
