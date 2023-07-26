4b221fc60e6479b6c341bbc7449759dee90228f6
lojo0307 <lojo0307@naver.com>
2023-07-26 14:02:51 +0900

    Feat: 실종동물 프로필 등록 - 이미지 업로드 추가
    Former-commit-id: f884904d0c9ddc53d8bb64fdd664722a0b270734

--- backend/src/main/java/com/moi/anitime/model/entity/profile/ProfileKind.java
+++ backend/src/main/java/com/moi/anitime/model/entity/profile/ProfileKind.java
@@ -1,6 +1,3 @@
-package com.moi.anitime.model.entity.profile;
-
-public enum ProfileKind {
-    DOG,
-    CAT
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:389f61bcfb327f30beb6702d3954b932b69f7c1757c3a02ff956b00681f7ccaf
+size 97
