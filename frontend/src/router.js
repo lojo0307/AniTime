1f7ce9b20be777f6d68e8772e385fd750e5c47c4
jaewoong <jwhur7@naver.com>
2023-07-26 10:51:31 +0900

    Feat: 입양 및 실종 페이지 라우터 설정
    Former-commit-id: 27feba78259b727f4f39b56391e5839989e5f4c2

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cca84a561f571cd63134cf916a124e60fff45ab17b9699518dc78a39ac074d1d
-size 724
+oid sha256:4271cc50b078094c7a994e217b2eea9480462001f87cb64fec8b22226a0c3368
+size 983
