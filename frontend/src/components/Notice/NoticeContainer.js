4f410e6b2dff1b6707451b2631cee909bc265f34
임성원 <dnjsndnjs@gmail.com>
2023-07-26 13:07:21 +0900

    Feat: NoticeItem 컴포넌트
    Former-commit-id: 74255c19b321a39b64c997f202680df2584a4bf7

--- frontend/src/components/Notice/NoticeContainer.js
+++ frontend/src/components/Notice/NoticeContainer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b93eb275f0f54d7724ead4f5c57a21672388d90ab4ce1da514f2ce74af9e7529
-size 1363
+oid sha256:fe59f8c133fd35dcf1b6e0ea731ab6b6758811c9eda6d0b4dcc0ec8659b6d326
+size 1974
