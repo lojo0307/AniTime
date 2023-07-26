f24ad11b138044bb077ac55cb12177cb184eab76
권지훈 <aspire51@naver.com>
2023-07-26 13:26:50 +0900

    feat : data update,insert Schdule
    Former-commit-id: 073bda4b780c33042986d0c1aaaa71cca9be74b2

---
+++ backend/src/main/java/com/moi/anitime/schedule/AnimalSchedule.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99f6a51cb8c7758a5b8ab90f4ce77e822f6701d9ba35c92659d2b5fc481e1f64
+size 1095
---
+++ backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4d57e43cdcc63e06e6580366f8ad381d6656df26e83740ee1a4929006bbc9c8
+size 485
