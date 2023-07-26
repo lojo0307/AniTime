f7eb117ab8255471cd8787363b40bc5507d648eb
임성원 <dnjsndnjs@gmail.com>
2023-07-26 10:55:13 +0900

    Feat: Modal 컴포넌트 생성
    Former-commit-id: c643d3979edca0bfa134eb9678dfab113292d3af

--- frontend/src/components/Notice/Notice.js
+++ frontend/src/components/Notice/Notice.js
@@ -1,24 +1,3 @@
-import React, { useState } from "react";
-import NoticeBody from "./NoticeBody";
-
-export default function Notice() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openNotice = () => {
-    setIsOpen(true);
-  };
-  const closeNotice = () => {
-    setIsOpen(false);
-  };
-
-  return (
-    <>
-      <img
-        src={`/icons/ic_notification.svg`}
-        alt="notice"
-        onClick={openNotice}
-      />
-      {isOpen && <NoticeBody closeNotice={closeNotice} />}
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e8b353444e2bc41ea851dfce7bced2b8a28726a7eeff142360bcb3068d345a2
+size 541
