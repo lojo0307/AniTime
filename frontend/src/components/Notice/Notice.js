57ae5c2bad3abf21415df6945791097a0a71c51e
임성원 <dnjsndnjs@gmail.com>
2023-07-26 13:53:43 +0900

    Feat: Modal 위치조정 props
    Former-commit-id: 2aceb9a6fe83a180399c78a372855139101084f9

--- frontend/src/components/Notice/Notice.js
+++ frontend/src/components/Notice/Notice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cac05dbbae697a9667587503c5c91a20c1ed5fced9cc509561983e50448f4f63
-size 596
+oid sha256:30291b566581406cba50e6560563b8da8033c67a44df3b48f143b37562b5e41e
+size 608
