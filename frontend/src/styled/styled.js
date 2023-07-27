214fa06db6d31df2be4901022aacea04b0c680f2
임성원 <dnjsndnjs@gmail.com>
2023-07-27 10:22:03 +0900

    Feat: MainContainer props 추가
    Former-commit-id: 4eeb4c97871fdc541c06744e718ef10954ac4b63

--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7464c71f17d1d50882107b0a9ad8ef232670cece6109ddd7607cb79cc9cc94
-size 829
+oid sha256:f1a9452146debfc075d425ea03ee0cb4ec4b984285500cf224212e300409644d
+size 947
