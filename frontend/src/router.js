ec2c81049c8f6e7039260acbcf507ed395cae066
임성원 <dnjsndnjs@gmail.com>
2023-07-27 09:40:47 +0900

    Rename: pages분리
    Former-commit-id: d9543bfba4043c42bde561c63349833b0f1a1941

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4271cc50b078094c7a994e217b2eea9480462001f87cb64fec8b22226a0c3368
-size 983
+oid sha256:9ac69fe8596cc6f3cbd9b26b15cdfede820c227c47ae09059e9a8bc141c9e874
+size 984
