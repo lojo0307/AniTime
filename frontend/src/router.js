91ddde3eff754a98d08a8319e1569cac881504da
jaewoong <jwhur7@naver.com>
2023-07-27 15:28:51 +0900

    Feat: 마이페이지 생성 및 하위 탭 페이지 라우팅 설정
    Former-commit-id: d086006175351931dfd40687e325f793f94e14d9

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b921dd4d4ebc509baf7770001d6ab70463f013a95763670db9ef9a5379946ddb
-size 963
+oid sha256:7badc6564b42638acd078f7a4b58530714538854efb633e6627c949ca2bc65b7
+size 1568
