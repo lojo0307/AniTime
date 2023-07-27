ddf613594e1bddc5aebe574009bdc58be867aa45
임성원 <dnjsndnjs@gmail.com>
2023-07-27 14:13:51 +0900

    Fix: MainPage 가로스크롤 안나오게 수정
    Former-commit-id: 1f1fed1e73e6a58b96d8a7d1b0fd362188ec0925

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a6f2125e400ee8d114b73a54f15e6790ad2981977f3f9bc7ebee7bb7f03cb1
-size 1851
+oid sha256:acab157c776d0f85a63fc723a7b09ee5dfe6b0cf866026d4c3692da26af9776d
+size 1921
