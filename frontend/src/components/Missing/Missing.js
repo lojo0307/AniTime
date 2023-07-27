e6b12f8afbd81345b2e09b49e32336b9299a3a94
jaewoong <jwhur7@naver.com>
2023-07-27 10:44:22 +0900

    Rename: pages 폴더 생성/ 파일명 수정 및 이동
    Former-commit-id: f7a3a1089ae5299d59ad3d7911c935b8c10efc6d

--- frontend/src/components/Missing/Missing.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d960f6159e375100b9d44c7f4f50ac304baee3c0f4bcb43f87cf7e6efdbd0f27
-size 1758
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4271cc50b078094c7a994e217b2eea9480462001f87cb64fec8b22226a0c3368
-size 983
+oid sha256:b921dd4d4ebc509baf7770001d6ab70463f013a95763670db9ef9a5379946ddb
+size 963
