32e6a0ae61e625212d0619f29e60d4d1bc0ebba6
임성원 <dnjsndnjs@gmail.com>
2023-07-27 09:41:32 +0900

    Design: 버튼, 메인 너비 수정
    Former-commit-id: a67687f17f9b7962eb4970857c5c0a48b7bbda52

--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62b6ddab109dca213e48b65c84eed17ba76694c30d6309f70e0519cf9f2b0d12
-size 810
+oid sha256:8c7464c71f17d1d50882107b0a9ad8ef232670cece6109ddd7607cb79cc9cc94
+size 829
