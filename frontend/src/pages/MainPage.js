1ae9ecf6cbe2e064564a4c419d193e425c748b8b
임성원 <dnjsndnjs@gmail.com>
2023-07-27 13:28:42 +0900

    Feat: MainPage 구성
    Former-commit-id: d0a2a6baa6308870c407dbfec6d87566ed096cdb

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,21 +1,3 @@
-import ModalTestButton from "components/Modal/ModalTestButton";
-import React from "react";
-import { styled } from "styled-components";
-import { MainContainer } from "styled/styled";
-
-export default function MainPage() {
-  return (
-    <MainContainer>
-      <MainTest>
-        <p>main</p>
-        <ModalTestButton />
-      </MainTest>
-    </MainContainer>
-  );
-}
-
-const MainTest = styled.div`
-  background-color: grey;
-  width: 100%;
-  height: 320px;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:32a6f2125e400ee8d114b73a54f15e6790ad2981977f3f9bc7ebee7bb7f03cb1
+size 1851
--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1a9452146debfc075d425ea03ee0cb4ec4b984285500cf224212e300409644d
-size 947
+oid sha256:77254abe7713808cfd29ed6163e585c87a9e61d8488fa772f21dde9a377a4824
+size 958
