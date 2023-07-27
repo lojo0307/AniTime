99db57d1d0c8ec2a8b47622c1949bcee89f1b5fd
임성원 <dnjsndnjs@gmail.com>
2023-07-27 16:20:13 +0900

    Feat: Slider 구성
    Former-commit-id: 4c0c1fed60dc91cf2b9527cff829045f6e7a6762

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acab157c776d0f85a63fc723a7b09ee5dfe6b0cf866026d4c3692da26af9776d
-size 1921
+oid sha256:7217139af7e4654dd861c2f866399042695ea00a119e8c65d1248bb5c8da63e8
+size 2084
