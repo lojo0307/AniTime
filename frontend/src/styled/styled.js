bebea3ad2c945e1750cfae0119c0ae47cccbcca3
jaewoong <jwhur7@naver.com>
2023-07-27 15:40:29 +0900

    Refactor: 마이페이지 코드 수정
    Former-commit-id: 17eeca1af13450626795cc699b0eb467e6acbffd

--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9073dd4c7333b05e737a817a26a9b773f920b71c11e8a4749a11534db669883e
-size 801
+oid sha256:b4b7e2a5adc5eff3061f0ffdcaabd3dd3a41511567908093f48e0a16f9daeddc
+size 802
