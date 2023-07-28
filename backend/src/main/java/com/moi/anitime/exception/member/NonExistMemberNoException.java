2645a50a7dbf65d0121353aa017e4a3af40d9111
lojo0307 <lojo0307@naver.com>
2023-07-28 17:53:08 +0900

    Feat: 프로필 삭제 및 조회 수정
    Former-commit-id: a2847330808667ec8f52eeb7631a3ecebe298713

--- backend/src/main/java/com/moi/anitime/exception/member/NonExistMemberNoException.java
+++ backend/src/main/java/com/moi/anitime/exception/member/NonExistMemberNoException.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f27229a51aa878327caaa5d589954501d79f57554d99fe0ab1ecc973cf4e811e
-size 360
+package com.moi.anitime.exception.member;
+public class NonExistMemberNoException extends RuntimeException{
+    public NonExistMemberNoException(){
+        super();
+    }
+
+    public NonExistMemberNoException(String message){
+        super(message);
+    }
+
+    public NonExistMemberNoException(String message, Throwable th){
+        super(message, th);
+    }
+}
