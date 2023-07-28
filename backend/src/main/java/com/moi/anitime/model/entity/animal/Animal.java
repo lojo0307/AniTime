0587f6237c53bf96c2b3ee40656229f29523be38
lojo0307 <lojo0307@naver.com>
2023-07-28 13:44:35 +0900

    Feat: 즐겨찾기 등록 및 해제 api 생성
    Former-commit-id: 1ec807b572c078f6f0cd5ed3c95075889a50e956

--- backend/src/main/java/com/moi/anitime/model/entity/animal/Animal.java
+++ backend/src/main/java/com/moi/anitime/model/entity/animal/Animal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eb038aa1fe2bad573391f8cd7cccace852ded3014004bf14b222425b4fcabf6
-size 1009
+oid sha256:2a35b8f17e69b5e17a38306ca2be41731ae5130f4cee4c57149048fd13bd5260
+size 924
