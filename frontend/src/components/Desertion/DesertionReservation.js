22738b73c91556195aef0a972dde0b1c7305dd5c
김정현 <prairial6@naver.com>
2023-07-28 16:53:41 +0900

    Feat: 입양 신청서 폼 디자인은 끝남
    Former-commit-id: c19e0d2b8be40f9b3b25957dbfa8dde4776928e6

--- frontend/src/components/Desertion/DesertionReservation.js
+++ frontend/src/components/Desertion/DesertionReservation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:260cf24c4068ff89b5fa07e56d29f6f1cf3f4363ce6d8e369928b21811c0de87
-size 2339
+oid sha256:acfa2a4eb393b69d845a111f48903b05fd8f26a7c5bac61f26d9a870f484addf
+size 2338
