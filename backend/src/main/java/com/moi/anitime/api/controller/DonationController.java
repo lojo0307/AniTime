82471223cc82b4fd324f48b612cd37aced05ff0b
임성원 <dnjsndnjs@gmail.com>
2023-07-30 21:56:10 +0900

    Feat: DonationBoard 조회 api
    Former-commit-id: 1417e07640ff9cd196e89d6631dcf6f14cf77870

--- backend/src/main/java/com/moi/anitime/api/controller/DonationController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/DonationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b36a7d046e8e959a796e9452737bcf1dbfde6e737f51082c45066fa421a5647
-size 1538
+oid sha256:9c915bb49a3d2523a453d90135f8cd7b4dcdc5304d86254ba2879a1cc80218bc
+size 3265
--- backend/src/main/java/com/moi/anitime/api/request/donation/DonationBoardRegistReq.java
+++ backend/src/main/java/com/moi/anitime/api/request/donation/DonationBoardRegistReq.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f911991e3c56bc88699b3011d1a49ca0958ba2380480b6aa8a2d8e756d009748
-size 1594
+oid sha256:e6c1187e588276fbbca8e6bfa2e3356d7434feb449646053d5ab3f17169e222b
+size 1508
