7ee87ae1966fa7f4cb54cf369941c11c4577fc21
임성원 <dnjsndnjs@gmail.com>
2023-07-31 15:56:08 +0900

    Feat: Donation Multipart 파일 이미지 업로드
    Former-commit-id: a166970f88ebd18cd0c50d36079242cf54c83d3a

--- backend/src/main/java/com/moi/anitime/api/request/donation/DonationBoardRegistReq.java
+++ backend/src/main/java/com/moi/anitime/api/request/donation/DonationBoardRegistReq.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dca3c13aac04673325a5d7f55f8ce6dfef5523798bd6011a0d1b1985765b4368
-size 1513
+oid sha256:7742b781135400cac2752fa7bc0d52c6fea6364e22dfe0f357b3cd95aee8e708
+size 1276
--- backend/src/main/java/com/moi/anitime/model/service/donation/DonationService.java
+++ backend/src/main/java/com/moi/anitime/model/service/donation/DonationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be1f420902d4a1f87e74d8a4c0d6bd718d90d82642a13480bbe39285b3fe77c2
-size 1383
+oid sha256:3323ba5edf5110e21d8b170b5b09fe10eff0752dd48d7e40cdf95db6df375c2b
+size 1524
