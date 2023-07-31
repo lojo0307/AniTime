8b8644f81b921fef1ede14e4559fe9bf8bcc4647
권지훈 <aspire51@naver.com>
2023-07-31 09:50:34 +0900

    feat :schedules
    Former-commit-id: c4e3192011b0a5d7f5eac9851e1540e7c5b21a52

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84c6a39010e4951f81dae83c1da2b1bf7f9292d4c27180dcc10eb4bee75bb04b
-size 8477
+oid sha256:e39a36a07fe611534f3c5b1456bd275adacf4362cf9b5a5d9e60e298195bc0a9
+size 8680
