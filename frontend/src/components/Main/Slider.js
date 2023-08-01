23a600b0d3d218857e9e6d538550be049bf1b170
임성원 <dnjsndnjs@gmail.com>
2023-08-01 12:18:27 +0900

    Fix: 슬라이더 경고 수정
    Former-commit-id: 120054d7467f615afe5ab6817742b00c4ecb2a3e

--- frontend/src/components/Main/Slider.js
+++ frontend/src/components/Main/Slider.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9f38d723c2fb684b91ea0f5124fba684a22500b84a124f60dc9f1e18c77aad2
-size 2686
+oid sha256:b5801ecc8cab8b36c0a0378b876ef5ec002d90eec17b86a03a55574744318f84
+size 2727
