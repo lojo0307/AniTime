68bcf81c6ee0fb01b3aa865af01edf0c52fafb29
김민태 <kimmintae981019@gmail.com>
2023-08-01 14:50:03 +0900

    Fix: 라우터 및 루트 파일 수정 (Exception 처리, 멤버 종류에 따른 화면 라우팅 위해)
    Former-commit-id: 9c2cf7f077786c3701932b65c5a22be5b861736b

--- frontend/src/App.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:521d19376d03cfdc73ef960aeda76206c443fb687b312d7a2d7c836160e659f3
-size 383
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bfc4d6793c42c7e5221f6a987203c948579517d78ee06edde4a69f577672a4c
-size 574
+oid sha256:3e274cf801961370ca2b6a6f4d54c56b93d6cc23f52eadfa3629b35032b0c6d4
+size 775
