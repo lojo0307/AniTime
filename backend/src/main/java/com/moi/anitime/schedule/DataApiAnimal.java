c3164b47b2970c2b386c8e0905ab49a4ff31a1f6
권지훈 <aspire51@naver.com>
2023-08-01 14:53:16 +0900

    feat : schedule
    Former-commit-id: 42e10344300130eef61762d6473c4190cefaeca9

--- backend/src/main/java/com/moi/anitime/schedule/DataApiAnimal.java
+++ backend/src/main/java/com/moi/anitime/schedule/DataApiAnimal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54fab3136d105c3a0fc086584fb7dc952b95b159db3e70791d7cf5ef9d78e10f
-size 12761
+oid sha256:b56a121d9355ab5b05a4ff3ffb9ba29e5e527d2063918466cb5e4948702ebe12
+size 13586
