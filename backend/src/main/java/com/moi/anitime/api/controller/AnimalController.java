4e157d3636b0e0a0865b8f179143bcb4f5d4f990
권지훈 <aspire51@naver.com>
2023-08-01 14:53:16 +0900

    feat : schedule
    Former-commit-id: 0bfccf13c7aa9ef2491fd84823fde8ad9504761e

--- backend/src/main/java/com/moi/anitime/api/controller/AnimalController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/AnimalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95edf27559fefb451296c7e054abf7fa84ebe7bd6df702d6a45a982a70d7a148
-size 2152
+oid sha256:391d24765a22813dc7092cca1a50ad18cd840681fc8a3f838fad8502fc6b62e9
+size 2257
