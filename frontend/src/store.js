43682b83cb0ce0ad20b143424e94249e637986f5
김민태 <kimmintae981019@gmail.com>
2023-08-01 15:58:41 +0900

    Fix: Redux 상태 값 로컬 스토리지에 저장 (로그아웃 시 초기화 필요)
    Former-commit-id: 9c4ef0f76d72c200e75226fbd1bb248e169885ad

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d8780a2af2b4f397bf5923a719a4fcb34ba6069d36f8feb30ac6b9a86fd0583
-size 192
+oid sha256:997cc470ec6bd6b85840857b8be85874a21454316706f2fcc93b5232e898e0b2
+size 202
