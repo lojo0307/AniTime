b2430a92664305ba6118df4b8a5c0a8e625ac9cd
임성원 <dnjsndnjs@gmail.com>
2023-08-01 16:51:16 +0900

    Feat: 로그인 안한 유저 리다이렉트
    Former-commit-id: 47352dd0e53a34372399a612d0895b4d69687ded

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e85e12ccc69a155dffa372f832bca90f993b752c4c28acf5e7dba10eb7d9a61
-size 2489
+oid sha256:9b6bd75e73ae4312e5698773f1890db2569db06063983010b825a66e3e385034
+size 2470
