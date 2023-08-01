d8457b9d6172de85c5b3dd64478cd9ab6116d662
권지훈 <aspire51@naver.com>
2023-08-01 15:23:45 +0900

    fix : deploy setup
    Former-commit-id: 17a8a7fd1ee64e0ad1547a0a84455c69192146ad

--- backend/src/main/java/com/moi/anitime/schedule/AnimalSchedule.java
+++ backend/src/main/java/com/moi/anitime/schedule/AnimalSchedule.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:942e0325f486b2454163050898f2da787d7239e8527ed32667a4a0e54ed6f192
+oid sha256:f2962bf1570a4c4339db731fbd1fb6a1bf50aa6fe549fc54c13e7e0a6a211667
 size 2779
