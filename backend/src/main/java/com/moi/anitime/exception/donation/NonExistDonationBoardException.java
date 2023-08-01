cdbb469a0cfcf61693d055f82ae658e0024d7a45
lojo0307 <lojo0307@naver.com>
2023-08-01 10:30:46 +0900

    Feat: 실종동물 프로필 상세 (클릭시 하위 컴포넌트 변경되도록)
    Former-commit-id: f17a1d76f077c433839de67acbc7e96ea67e9d93

--- backend/src/main/java/com/moi/anitime/exception/donation/NonExistDonationBoardException.java
+++ backend/src/main/java/com/moi/anitime/exception/donation/NonExistDonationBoardException.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e1a075403212d78ae273f1f63afecb4f6c70b21f00ecf24fd66dec2b50b139e3
-size 384
+package com.moi.anitime.exception.donation;
+
+public class NonExistDonationBoardException extends RuntimeException {
+    public NonExistDonationBoardException(){
+        super();
+    }
+
+    public NonExistDonationBoardException(String message){
+        super(message);
+    }
+
+    public NonExistDonationBoardException(String message, Throwable th){
+        super(message, th);
+    }
+}
--- frontend/src/components/Profile/ProfileDetail.jsx
+++ frontend/src/components/Profile/ProfileDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fee188f88f534e368fc4223e6c8c1bfd60380eb0be8f6a6f847fbde0db0dd1c0
-size 274
+oid sha256:002cc1df57473e55c856d84f2241f5b02629b1f6e7af3e2a49e5b08d50ab4af1
+size 214
