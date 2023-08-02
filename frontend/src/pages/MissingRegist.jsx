7bfcec678f4efa0589b7683ae943070732eaadbf
lojo0307 <lojo0307@naver.com>
2023-08-02 12:10:44 +0900

    Feat: 실종동물 등록 페이지 생성
    Former-commit-id: 494552594f26655b37960649141c909142aec43f

---
+++ frontend/src/pages/MissingRegist.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4eca3f5beca82595630e5797dfdb0bfdaf5c79cf9396bff08803658f00dc28f
+size 13967
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e85e12ccc69a155dffa372f832bca90f993b752c4c28acf5e7dba10eb7d9a61
-size 2489
+oid sha256:a883812f131c1d483655ef5cf766b67601580e81813e2f8997bf585a03322fc5
+size 2796
