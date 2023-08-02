6489bc2c8ed8f498454a1184b9316730c98fc959
lojo0307 <lojo0307@naver.com>
2023-08-02 17:54:37 +0900

    Feat: 실종동물 등록 페이지 지도 추가하는중...
    Former-commit-id: 3b2a22fe7d05e4ee2b4ead4791733538eb5f396b

--- backend/src/main/java/com/moi/anitime/api/request/meeting/MeetingReq.java
+++ backend/src/main/java/com/moi/anitime/api/request/meeting/MeetingReq.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e45b13fa796995c6370d359436bbe8c53ebe11b9eb86e425f639d08098277a40
-size 280
+package com.moi.anitime.api.request.meeting;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("meetingReq")
+public class MeetingReq {
+    int desertionNo;
+    int generalNo;
+    String img;
+    String reservedDate;
+}
--- frontend/src/components/MyPage/MyPageWatchlist.jsx
+++ frontend/src/components/MyPage/MyPageWatchlist.jsx
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c7c978d826a74e5113912749ef9f7ab32d2edfc546965f89d25880f9d6a04824
-size 98
+import React from "react";
+
+export default function MyPageWatchlist() {
+  return <p>즐찾</p>;
+}
