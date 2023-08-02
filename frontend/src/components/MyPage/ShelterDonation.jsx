9d7058c7279130865362243f53a202cd639e031d
임성원 <dnjsndnjs@gmail.com>
2023-08-02 10:55:20 +0900

    Feat: 보호소 메인페이지 컴포넌트 생성
    Former-commit-id: 0caa9ecd8e548bf0070aeb3d8e7f55201153605b

---
+++ frontend/src/components/MyPage/ShelterDonation.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2404af99f2462d0d4c55e6e60bba6f6898c378e22082662e20fb34a58c2787d
+size 105
---
+++ frontend/src/components/MyPage/ShelterMeeting.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:02fc95e10df13b4231e905e34e870e59ee4565b650601799f702689ebf49bb6a
+size 114
