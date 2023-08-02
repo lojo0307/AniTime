72bf40d9e72f343e2bebf84dc60e23ce455ce10f
임성원 <dnjsndnjs@gmail.com>
2023-08-02 13:24:59 +0900

    Feat: 보호소 메인페이지 미팅 내역 구성
    Former-commit-id: f3b0fd8ec94070311b2629cc4df0f8a16e5bb55f

---
+++ frontend/src/components/MyPage/MeetingList.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b164215f74a4cf42141ec81747dba4a6c1186a8bf8b04bafe3496ec09d395e24
+size 1607
--- frontend/src/components/MyPage/ShelterMeeting.jsx
+++ frontend/src/components/MyPage/ShelterMeeting.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02fc95e10df13b4231e905e34e870e59ee4565b650601799f702689ebf49bb6a
-size 114
+oid sha256:04fe7b012e66c6446fb4896f662caad406fc106e416165eb53e40a6f193fadc8
+size 608
