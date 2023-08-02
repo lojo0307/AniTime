1281efb4e2ebf8acb730a33451d38cdd090bef96
권지훈 <aspire51@naver.com>
2023-08-02 09:38:30 +0900

    fix : schedule fliter
    Former-commit-id: 71a969bf2bc104580ee508ca0f9233475e21add7

--- backend/src/main/java/com/moi/anitime/schedule/DataApiAnimal.java
+++ backend/src/main/java/com/moi/anitime/schedule/DataApiAnimal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0013138a0a119c162607e5dbfd14900e3807c090d0581c3fcae242032fe340e5
-size 13600
+oid sha256:033993a23aa24a721bae08f59062e35538c2697b98fd6478a5abd5bddcb2d2b0
+size 13759
--- backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
+++ backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e26f7ef4372a82ba66773a162f5b190e1efcfeb71c9de208c9244b1ac68f924
-size 6625
+oid sha256:e1a78481ce866d625adb2dbebbd006e419245b808bef8efffef923d1190092db
+size 6867
