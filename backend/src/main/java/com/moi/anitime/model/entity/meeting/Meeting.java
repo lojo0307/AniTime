b49299ea209cf9b336f9ec1325e0bf8f00d7d96f
권지훈 <aspire51@naver.com>
2023-08-02 14:39:05 +0900

    fix : entity bug fix
    Former-commit-id: 0cc5d75d2588d8ade273c47711f3565669beed4b

--- backend/src/main/java/com/moi/anitime/model/entity/meeting/Meeting.java
+++ backend/src/main/java/com/moi/anitime/model/entity/meeting/Meeting.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c059166954ae9f4c78cd230f1c6b0ff9693238982a65c2a10269462e83ada1bc
-size 537
+oid sha256:ab6167ceb7fb02fadd0d1f5e6d32aeb8fbe990aaee62e934c892912b691e9776
+size 554
