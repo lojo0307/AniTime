918dbfd7f720f568baae0f1586504deed4e0d626
권지훈 <aspire51@naver.com>
2023-08-02 09:38:30 +0900

    fix : schedule fliter
    Former-commit-id: 8bac9e0321b02b64f003ed7e8f2be3797a619265

---
+++ backend/src/main/java/com/moi/anitime/api/response/animal/AnimalListRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f90ee58a491f9ba238bcd019c9b7a88f52eaf9e1f539e8d47cfd2a481b1d2f03
+size 77
---
+++ data/Table_dummy_query.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f3fea2c93287310a269caabb0d4df6882ac94d42c6506c074ca646795789f4d
+size 14460
