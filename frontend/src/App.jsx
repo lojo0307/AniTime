e97146f511e7aac57dba8dc5d7532f0054b742ce
김민태 <kimmintae981019@gmail.com>
2023-08-02 10:18:05 +0900

    Fix: Redux persist local -> session
    Former-commit-id: 70e402cb4b7dc5d7c40398e594c0279b49e1ac57

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1fe48d8ddad9f99fea0bc91abc0c40e62d2255b37f8e8dabd4b8eb4e3b3df413
-size 257
+import Footer from 'components/Footer/Footer'
+import Header from 'components/Header/Header'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function App() {
+    return<>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+}
