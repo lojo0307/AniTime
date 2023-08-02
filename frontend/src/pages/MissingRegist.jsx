905c99b5ee050c42006c129747116305ffe3b791
lojo0307 <lojo0307@naver.com>
2023-08-02 17:54:37 +0900

    Feat: 실종동물 등록 페이지 지도 추가하는중...
    Former-commit-id: 13a6c7817c0e8dc29876d6d1d032ce4f383aada5

--- frontend/src/pages/MissingRegist.jsx
+++ frontend/src/pages/MissingRegist.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:887e914d3b672f159b691c4149fd9292228db890a2ff941fa26bbae64e5db4e2
-size 14798
+oid sha256:005b58f032fa75e0680a4d50f1cd2e9d154d934871210ba17bf967180cb82253
+size 16026
