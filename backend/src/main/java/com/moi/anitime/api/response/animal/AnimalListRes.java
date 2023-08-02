1e41b2be76eed1401b3a4bcf56c30d42417fff71
권지훈 <aspire51@naver.com>
2023-08-02 14:10:12 +0900

    feat : openvidu controller
    Former-commit-id: c46b07558a6adfe8f753785cf58027a4880f225e

--- backend/src/main/java/com/moi/anitime/api/response/animal/AnimalListRes.java
+++ backend/src/main/java/com/moi/anitime/api/response/animal/AnimalListRes.java
@@ -1,4 +1,3 @@
-package com.moi.anitime.api.response.animal;
-
-public class AnimalListRes {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d0895601302bc571687b402f14e5fd82ceedc265a4980a4314637769deda893
+size 78
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,3 @@
-import Footer from 'components/Footer/Footer'
-import Header from 'components/Header/Header'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-
-export default function App() {
-    return<>
-    <Header/>
-    <Outlet/>
-    <Footer/>
-    </>
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1fe48d8ddad9f99fea0bc91abc0c40e62d2255b37f8e8dabd4b8eb4e3b3df413
+size 257
