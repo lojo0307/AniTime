ac355560df26e09aea958ce26beb3613ef2a3224
장명주 <lojo0307>
2023-08-03 23:53:58 +0900

    Fix: 프로필 상세조회 시 등록된 프로필이 없으면 등록 페이지로 넘어가도록 수정, 프로필과 유기동물 상세조회 시 이미지가 없으면 기본 이미지가 조회되도록 수정
    Former-commit-id: 0a8df31b35c0ed5d5a547a16daf42bb64eace80f

--- frontend/src/components/Desertion/DesertionDetail.jsx
+++ frontend/src/components/Desertion/DesertionDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef39f103fc1cfeeb02563a459f60646913bf64c95c69b8d298d80e1399a17d4
-size 6155
+oid sha256:739e3bc47bbb08bd73b3c1cfee7b36366cf0b2c06838a84338a80dee2c75a2a8
+size 6297
