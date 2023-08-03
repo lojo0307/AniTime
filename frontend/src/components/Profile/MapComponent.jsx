6eb0fdce6981993bbf4a2eb1fd0090a5ba7b76bb
장명주 <lojo0307>
2023-08-03 21:21:01 +0900

    Feat: 실종동물 페이지에 등록 버튼 추가, 수정 및 삭제 버튼 활성화
    Former-commit-id: 770ce02ed9cbf01217275c424225714ee298d0b7

--- frontend/src/components/Profile/MapComponent.jsx
+++ frontend/src/components/Profile/MapComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:164e4aa801d469fd1507c3b7e51ac44d95e1d365c318cb7cc634e7b868b1a2ce
-size 8152
+oid sha256:b5774ce82424d4b8da904f8d5119ee1831f08a0e559a1d4d74a3dcf1a283ff46
+size 8114
