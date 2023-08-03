69c8fdc9f7dd0ca3fad7f3d01d6039a60d31edc9
lojo0307 <lojo0307@naver.com>
2023-08-03 17:58:32 +0900

    Fix: 프로필 상세 css 수정
    Former-commit-id: 54c0c135b78840be2aadef779d3369ff5c038360

--- frontend/src/pages/MissingPage.js
+++ frontend/src/pages/MissingPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3409806b414382c21077a90d3b7a38098257adc7c45c434bd410cf93ce040b98
-size 2786
+oid sha256:e175ba4c2df89e3a6891c03b62d474ca200d49f981953accb037b10f7f457ece
+size 2615
