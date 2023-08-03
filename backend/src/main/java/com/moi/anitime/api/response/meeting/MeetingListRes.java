4592c612ba7d518438e153f61df1a0801a32bdd9
김민태 <kimmintae981019@gmail.com>
2023-08-03 10:53:50 +0900

    Feat: 일반회원 Meeting List 조회
    Former-commit-id: 75902ab27b9c268bae95434f7c04940a781f12bb

--- backend/src/main/java/com/moi/anitime/api/response/meeting/MeetingListRes.java
+++ backend/src/main/java/com/moi/anitime/api/response/meeting/MeetingListRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:feefb1d63df7a49becd09a94e0a8c89eae8698619b9701f323e47ba07f2c144e
-size 345
+oid sha256:e65e55b7fbf7edaa9945dd57f6ab4bdc42e864e1615fa0986398aee4a12e69a6
+size 352
