1bb5aa1d8e50942e2564a72f45cc65ab3115e0eb
김정현 <prairial6@naver.com>
2023-08-03 15:51:08 +0900

    Feat: 알림 생성
    Former-commit-id: e6ab74f0ae0c5a498c1756c2b4758982eb395f90

--- backend/src/main/java/com/moi/anitime/model/repo/ChatMessageRepo.java
+++ backend/src/main/java/com/moi/anitime/model/repo/ChatMessageRepo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d919ce97bfee76873c14d2d5b596eb745a9f3910b86a2b915c41ab853fcae01
-size 704
+oid sha256:7be96535a25c6fe0b47d0472369120ea2cd969f30fbcf4720a28e829e1f93f36
+size 980
