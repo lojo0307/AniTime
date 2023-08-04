dd16bf0e4aeaf72d3132c166c1c6f2a8e42c3222
권지훈 <aspire51@naver.com>
2023-08-04 13:17:39 +0900

    Update data/Table_create_query.sql, backend/src/main/resources/application.properties
    Former-commit-id: e655ddbe8d75f3efa5929a3bea7a9386ecafa437

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28adea2030380f0537dd85d8d73ee6d8b0ef1efc6bd7e604edaa06fdcde612fe
-size 8410
+oid sha256:e43619767682fdc51cc71f06130b6ad69de6055e4680dd1c49b2765bbe12f544
+size 8412
