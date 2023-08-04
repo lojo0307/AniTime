0554055ddc2482cdcfed41cc1382bff2e1b9b162
임성원 <dnjsndnjs@gmail.com>
2023-08-04 17:53:47 +0900

    Feat: 미팅창 비디오 음량 컨트롤
    Former-commit-id: d8b1551ce1744ee858a084d5d6861c0478a907c0

--- frontend/src/components/Meeting/OvVideo.jsx
+++ frontend/src/components/Meeting/OvVideo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1395a3d4c10606b6fca73a3772a27930775684cb20e56560cb93f808e2a99f28
-size 704
+oid sha256:b3c65205bfce95c36b0c337512e207296f56e433c3a3e499ebe19d2d6dd4bab9
+size 912
--- frontend/src/pages/Openvidutest.jsx
+++ frontend/src/pages/Openvidutest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c8cd1057e5b68fcd22c9116ffe5c7d1a2474165faa33f954be063c9550184cf
-size 6609
+oid sha256:f16402e24e13755c2258f111d1b7ee73c7935fba9528080a120ac5c5b6b83575
+size 6535
