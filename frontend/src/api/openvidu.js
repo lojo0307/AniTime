9fbcabc24c340e89445b210581b9d1a116991790
임성원 <dnjsndnjs@gmail.com>
2023-08-04 12:28:27 +0900

    Feat: openvidu 테스트
    Former-commit-id: d151ada9a5fb2465410f0d2dce545f08770c3310

---
+++ frontend/src/api/openvidu.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a216cf86796b7b49512ecbfab8db1cbd0902d9d4f5ce5c7aeca6b4ee1916cb4c
+size 827
--- frontend/src/pages/Openvidutest.jsx
+++ frontend/src/pages/Openvidutest.jsx
@@ -1,17 +1,3 @@
-import React from "react";
-import { styled } from "styled-components";
-
-export default function Openvidutest() {
-  return (
-    <Div>
-      <p>Router Test</p>
-    </Div>
-  );
-}
-
-const Div = styled.div`
-  background-color: blue;
-  margin: auto;
-  width: 1000px;
-  height: 1400px;
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c7c0f416d6b9f07e20da5792198b25108a481219678abdf355dbb736ac9fe5e
+size 5261
