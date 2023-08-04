070c02661b7c2b69ee148cd584fd442920a6c715
임성원 <dnjsndnjs@gmail.com>
2023-08-04 12:28:27 +0900

    Feat: openvidu 테스트
    Former-commit-id: a1258fe89c4412f56ecf062f3cda67fd60565e05

--- frontend/src/pages/Openvidutest.jsx
+++ frontend/src/pages/Openvidutest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0889bd98e58c9d2a04b18a2a6d858fd4c8dc248e75652d5d1200c575520abaed
-size 282
+oid sha256:5c7c0f416d6b9f07e20da5792198b25108a481219678abdf355dbb736ac9fe5e
+size 5261
