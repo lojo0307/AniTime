fd392bf72f6d72b254aed4df15f72c6a1682029f
김정현 <prairial6@naver.com>
2023-08-04 17:30:21 +0900

    Feat: 이미 예약된 날짜 선택 불가능하게 하는 기능 연결
    Former-commit-id: 066c29678cf0c5a113d8ff1ca169d70ad737fbc0

--- data/Table_create_query.sql
+++ data/Table_create_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28adea2030380f0537dd85d8d73ee6d8b0ef1efc6bd7e604edaa06fdcde612fe
-size 8410
+oid sha256:de215df9f4ef58120ba94d7658474fb2638443ca8c6c76f0e40c9473ada7f1f8
+size 8425
--- frontend/src/components/SelectTime/ReservationForm.js
+++ frontend/src/components/SelectTime/ReservationForm.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d91c91d88a2d2a7c2b1ff565cea760cbd16010f72e1cf43422edc4dbba06c2
-size 8428
+oid sha256:5f5c3c6069b3b96aec1651e6d4b524e1d1a5c0fe29c4672d373d3b2d61d5ad3d
+size 8429
