cb42fdec527aa4f023be746f4da65fc645665545
임성원 <dnjsndnjs@gmail.com>
2023-08-04 13:58:19 +0900

    Feat: 미팅창 헤더 푸터 구성
    Former-commit-id: b70db2b6db1db1961ce01250da907a64861a5e96

--- frontend/src/components/OpenVidu/OvVideo.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5f50a234911978fabed45522d2b1ebc229b60a7d5983c76ba4b33a7f17e130cc
-size 655
--- frontend/src/pages/Openvidutest.jsx
+++ frontend/src/pages/Openvidutest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c7c0f416d6b9f07e20da5792198b25108a481219678abdf355dbb736ac9fe5e
-size 5261
+oid sha256:ab151ae0b7f786cca97aee216ac30abf7280dba16b85dd103be60814a5e1066b
+size 5329
