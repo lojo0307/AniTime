49454dcbf60ed086d0a32321d1cd07081319e03b
lojo0307 <lojo0307@naver.com>
2023-08-04 16:15:21 +0900

    Fix: 유기동물 및 실종동물 조회시 만 나이 -> 출생년도로 변경
    Former-commit-id: f2bf52befd822d2e125aae741181467d243f0943

--- backend/src/main/java/com/moi/anitime/api/controller/AnimalController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/AnimalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db43654eef577bde09899ef8aaa2c8373e6a323fd013b8a5a878a8e7cd123453
-size 4370
+oid sha256:8271a715e0c43ca48f831836bfc958beaf53bd99f2df52500cea10ae3b266600
+size 4386
