9f9983bdd73f06a3d49a3479ebb2443c4fed4be0
장명주 <lojo0307>
2023-08-06 02:59:47 +0900

    Feat: 보호소 회원 마이페이지 - 후원 공고 목록 페이지 생성
    Former-commit-id: f5f32d904da4edca5df6c2b5ff024ec273c89a0e

--- backend/src/main/java/com/moi/anitime/model/service/donation/DonationServiceImpl.java
+++ backend/src/main/java/com/moi/anitime/model/service/donation/DonationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b70382d9839480ceb8ebc9b164b30b7acac46d23f99fbcb01d3bd194a7e510e
-size 8997
+oid sha256:19b5f323137abea3e1426947b34647a383ac6fd0db92c30367f8aae53e94801c
+size 9054
