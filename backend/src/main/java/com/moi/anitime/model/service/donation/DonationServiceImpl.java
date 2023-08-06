9400eb92f9cadc01dcd3abb4775102844b666e47
김민태 <kimmintae981019@gmail.com>
2023-08-06 15:54:34 +0900

    Feat: 간편결제 API 구현. toss로 사용
    Former-commit-id: dd3b67619190e322aeb151e4ec6bd3005badb135

--- backend/src/main/java/com/moi/anitime/model/service/donation/DonationServiceImpl.java
+++ backend/src/main/java/com/moi/anitime/model/service/donation/DonationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9ad3041657288e535769fdf4879e0122a0138357a0efb73efbd5879fb076dea
-size 11423
+oid sha256:6e3740559f74172d53e535e4ac64482473efdcc59ee5869d6845216be0fad4bc
+size 11533
