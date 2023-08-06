1f383f0d74a92b4a6356e9fecfed644bf89b8ed0
장명주 <lojo0307>
2023-08-06 10:39:00 +0900

    Feat: 보호소 마이페이지 후원공고별 후원내역 조회 페이지 생성
    Former-commit-id: b8bd9c2116cf387c197a75412c74769e15b6a719

--- frontend/src/components/MyPage/ShelterDonationDetail.jsx
+++ frontend/src/components/MyPage/ShelterDonationDetail.jsx
@@ -1,8 +1,3 @@
-export default function ShelterDonationDetail({ boardNo }) {
-  return (
-    <div>
-      하이
-      <div>{boardNo}</div>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d02ee266698cd4d59bd3b61d50e21d2789af7e4584e6e53ec8974b9a8e2c0a1
+size 4339
