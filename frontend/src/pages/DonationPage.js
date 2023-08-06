e8775fcb7534b640f3ed45a5ddce997e23bcd711
김민태 <kimmintae981019@gmail.com>
2023-08-06 15:54:34 +0900

    Feat: 간편결제 API 구현. toss로 사용
    Former-commit-id: aae37e14fb9f860fd777ab715850f4accde1b739

--- frontend/src/pages/DonationPage.js
+++ frontend/src/pages/DonationPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8291a47bf8d06afef852c82941fd8724930ba168da763851740185c19a85953
-size 2075
+oid sha256:3d8fbb3793d7c9d7f560095eccc1a0c2eebc04cd81bbbda722a446842b3bf4f3
+size 2403
