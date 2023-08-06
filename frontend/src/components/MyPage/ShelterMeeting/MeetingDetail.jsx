96fddc80f1d5d16f52caba57b99a1322ab79b934
임성원 <dnjsndnjs@gmail.com>
2023-08-06 19:41:09 +0900

    Feat: 보호소 마이페이지 미팅내역 api 조회
    Former-commit-id: 2ce5d556ae43a75e4be70558eff59439b92dc8c5

--- frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
+++ frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b34884fc3bf461c536be2cea280220f2f9755bbcc14e81b54a7e5a27b3872b1b
-size 2809
+oid sha256:008c3fb442275db50adae56984f4b1e24265802ba3c93c16b87eb0f970e34e33
+size 2783
