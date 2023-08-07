c2e931370114fe88bc07076f5120a5ccb6912d68
lojo0307 <lojo0307@naver.com>
2023-08-07 14:23:58 +0900

    Feat: 실종동물 등록 페이지 생성중 ...
    Former-commit-id: 26e3b679718b7adbb4d3bfaffadf9e7f4c889ec1

--- backend/src/main/java/com/moi/anitime/exception/notice/CountNoticeException.java
+++ backend/src/main/java/com/moi/anitime/exception/notice/CountNoticeException.java
@@ -1,15 +1,3 @@
-package com.moi.anitime.exception.notice;
-
-public class CountNoticeException extends RuntimeException{
-    public CountNoticeException(){
-        super();
-    }
-
-    public CountNoticeException(String message){
-        super(message);
-    }
-
-    public CountNoticeException(String message, Throwable th){
-        super(message, th);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a83992750cb3a3f59e205ab162ba564a8bc1e3cf837e4b4247c6c196631fa5b1
+size 341
