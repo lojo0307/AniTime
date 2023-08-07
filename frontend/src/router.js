c07edfae97c75cabbd2ffc028a177f004e15fa00
lojo0307 <lojo0307@naver.com>
2023-08-07 14:23:58 +0900

    Feat: 실종동물 등록 페이지 생성중 ...
    Former-commit-id: f794c12701becd2578bd52b642fce11d94bdfc2d

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac47fa40be20822f1fdb6f31d232e6b39669bc97b1e76f83fe5f0c220d531323
-size 4051
+oid sha256:977505b3fb83089c525127a89ca7d0eac20e720f73f57eb83ae04fa04c33b529
+size 4054
--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:888941c70824d17c42505c79e76f9179df9f7c4682339c40cc81ff0f419eb7b9
-size 1874
+oid sha256:7e9e1ed11fdf0ae70983dc2583aab8d93cfbcbc25203cd886799d2b6fd9d6d1b
+size 3638
