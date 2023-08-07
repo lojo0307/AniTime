f9a28c94f793cadb3cfa7498f168acc5bf61bd2d
권지훈 <aspire51@naver.com>
2023-08-07 15:49:54 +0900

    feat : Data dummy update
    Former-commit-id: e670b90cf66a1709e4f69439ed2130f16d299a4a

--- data/Table_dummy_query.sql
+++ data/Table_dummy_query.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:024e02be9907b92c91d67fa0150340c94a24a2f26e66efc8dbf33a9b6eb18f76
-size 75226
+oid sha256:edb30ce0e7f306965e7e2dcc414a763c15326d895aeab917a73ea143ce06e067
+size 2631448
