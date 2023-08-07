fed20e0a61f71e2d4635090b59ce6c9a2c593240
권지훈 <aspire51@naver.com>
2023-08-07 17:45:12 +0900

    feat : data dummy update
    Former-commit-id: 1c2b2d30b00c543e1efdd4eeae4bb402768660c1

--- backend/src/main/java/com/moi/anitime/api/controller/OpenViduController.java
+++ backend/src/main/java/com/moi/anitime/api/controller/OpenViduController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58ebdf97decd79964aac4d9d7c0ad2928a30195d71fcdbaea7eecb0233b9cc65
-size 2721
+oid sha256:7f55c02643c6d28bb7e101f344d8df5768c13f11f30e8f35bc942d37cba0ec4f
+size 2722
