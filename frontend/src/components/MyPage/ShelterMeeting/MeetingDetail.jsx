9efc5ebd796b92d20fc899130bcf84ce15102415
임성원 <dnjsndnjs@gmail.com>
2023-08-07 14:44:00 +0900

    Feat: 보호소 마이페이지 승인 반려 입력폼 생성
    Former-commit-id: 416e84b90a41adc996e9f834e15cf3dc604c426f

--- frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
+++ frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bac3e7615f8fbbb640896237fc890b02f9b8bd0ad3d17fd1babf511dc616fd7
-size 3815
+oid sha256:85971c59457549ca7d1d9d33371638e51fdfbd88cf5fe918d1eb46fed3a407bf
+size 6650
