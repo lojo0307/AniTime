b841bbd76477b26699513212ee5494ae7fdc9e9d
DESKTOP-1JVI7QI\SSAFY <prairial6@naver.com>
2023-08-07 14:46:05 +0900

    Feat: FAQ 페이지 완성
    Former-commit-id: d3982f1273990b4e37c47c9147f5ae3fed36103b

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac47fa40be20822f1fdb6f31d232e6b39669bc97b1e76f83fe5f0c220d531323
-size 4051
+oid sha256:2a0ecd1101d81935e16fc42499f40af10dab7be3a66fb9d44ea0931bbe20c681
+size 4316
