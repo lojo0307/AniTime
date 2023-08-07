df1878cb41ff2c4242d278590e21837f12b48d2b
임성원 <dnjsndnjs@gmail.com>
2023-08-07 15:32:48 +0900

    Feat: 보호소 마이페이지 미팅내역 승인 반려 기능
    Former-commit-id: 3336734027c5184ea8327af869331127c6a81a0a

--- frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
+++ frontend/src/components/MyPage/ShelterMeeting/MeetingDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85971c59457549ca7d1d9d33371638e51fdfbd88cf5fe918d1eb46fed3a407bf
-size 6650
+oid sha256:38d7177481145abdab62e57ead5f3fec5bf7256f3199f5bb901f00d7274703d1
+size 6653
