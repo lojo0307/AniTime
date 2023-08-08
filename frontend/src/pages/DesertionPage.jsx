cf0d6f84385d272002397086e06c821a5dcfd819
Jaewoong Hur <jwhur7@naver.com>
2023-08-09 06:08:52 +0900

    Feat: 채팅 unreadCnt 업데이트
    Former-commit-id: e69eca5ab7d6cf18418473435117fda071077c9c

--- frontend/src/pages/DesertionPage.jsx
+++ frontend/src/pages/DesertionPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1411c668515298c5d41368eea50fe86efe31e1867e4772cb79c4b09226fd258a
-size 6284
+oid sha256:c0d9388232a11659c860e48da2c8f22ed8d6415431b3db4e9fd7174be8fd4986
+size 6322
