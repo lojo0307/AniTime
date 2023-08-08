23b6db82f2729a0faa937932935f628e06944e79
김정현 <prairial6@naver.com>
2023-08-08 21:45:08 +0900

    Feat: 메인 페이지 동물 마리수 구하기
    Former-commit-id: 28021cd6eb022640e0c98415cc461a998038e331

--- backend/src/main/java/com/moi/anitime/config/SecurityConfig.java
+++ backend/src/main/java/com/moi/anitime/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8d32ed5e07141585e2a08f0c10dc2820f0e4dc30214820eade20e4a46bc63e
-size 3096
+oid sha256:5f10f9592aae3cf8c774012d1e640d9915cdf8c1bc14cfd9a751082c3911f7a6
+size 3230
--- backend/src/main/java/com/moi/anitime/model/repo/AnimalRepo.java
+++ backend/src/main/java/com/moi/anitime/model/repo/AnimalRepo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:516a36db2ff97883212e374e45a46b30099a662fe55188d2f431dd77a81a8d1b
-size 2483
+oid sha256:06af8e6890feefb411020e65c2da4362aec62ee59030931656365d969e7cf44a
+size 2929
