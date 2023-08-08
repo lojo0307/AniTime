fe6bd39b903b803885317d393e42642a40947373
장명주 <lojo0307>
2023-08-09 00:30:56 +0900

    Feat: SelectBox 생성
    Former-commit-id: 1ce85095c2a341cd1292fc7a07fecde374398ac3

--- frontend/src/pages/DonationRegist.jsx
+++ frontend/src/pages/DonationRegist.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2d16769b0581146b29aa96c7dd47cc9f68ecf86e245755a5f80db4764ae1bb2
-size 9678
+oid sha256:b0d267469645c23d3afa3d58ba0ad9c49e095cb1e31732cdadc51384c1a3e5ed
+size 10025
