f03ad8fb3d695b72f4e489de79f88f2c83f3bf1c
jaewoong <jwhur7@naver.com>
2023-08-08 12:32:31 +0900

    Refactor: 마이 페이지 채팅
    Former-commit-id: 9463c5003c95e61ddd794e2cf5b4a266a810e908

--- frontend/src/pages/DonationPage.jsx
+++ frontend/src/pages/DonationPage.jsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f3758eefcc6193e65fbccbbf867c7b07388905415391dcc32bcae8d2dd3c603
-size 165
+import React from 'react'
+import Payment from 'components/Donation/Payment'
+export default function DonationPage() {
+  return (
+
+    <Payment>
+
+    </Payment>
+  )
+}
--- frontend/src/styled/styled.js
+++ frontend/src/styled/styled.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93e43f93231f5ce277e99297924a90a20d51dc4c99226a842753f740f58d1ba2
-size 3539
+oid sha256:deb27a1a41258ef456bdacd83ca4ee02d2b3632f2576b56cda548fdcd62874e8
+size 3534
