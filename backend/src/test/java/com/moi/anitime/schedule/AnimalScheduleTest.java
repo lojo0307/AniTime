b18f1f1bf288668249c63d1492be9faa957a463d
권지훈 <aspire51@naver.com>
2023-08-08 09:14:05 +0900

    Update backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
    Former-commit-id: a5a4b20dfc006070a0288921f2bc3822fddf2ffe

--- backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
+++ backend/src/test/java/com/moi/anitime/schedule/AnimalScheduleTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:633c2799c9913aed8255d2f13905f41e490ba23dbd2961d4a5f80ab110ba1d2a
-size 8085
+oid sha256:7cde400a1198f598bf677a6dc3778f13b294c2e618bae94babe6fcfb2b97bd3d
+size 8131
