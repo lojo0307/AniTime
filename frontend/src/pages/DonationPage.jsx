614ba4fe4109adcd4fd1563f58814759da276ac4
lojo0307 <lojo0307@naver.com>
2023-08-08 14:21:13 +0900

    Feat: 후원하기 목록 페이지 생성
    Former-commit-id: 8f8cac73781c5149846f08efb16acff171909d8f

--- frontend/src/pages/DonationPage.jsx
+++ frontend/src/pages/DonationPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f3758eefcc6193e65fbccbbf867c7b07388905415391dcc32bcae8d2dd3c603
-size 165
+oid sha256:da552cbe3fa9b700a618fb7a3845a556e7586db0cac6d1612fa47020cb2cf9f0
+size 7503
