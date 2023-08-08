cc13f49e8441efee9a98cc65d602d92cfa91afd6
권지훈 <aspire51@naver.com>
2023-08-08 13:02:46 +0900

    fix deploy
    Former-commit-id: 7b9a44a848a5029301bd1040f8e3b43a599f6da0

--- frontend/src/api/commonHttp.js
+++ frontend/src/api/commonHttp.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e53a6b45f5c11322221a9950fa0934a2f6618b7db946bed66ec8093c3f49169a
-size 949
+oid sha256:6a0a84474a7ff5a345c42dae633f84ef20318740d745e83bee94f2a6791f8bea
+size 948
