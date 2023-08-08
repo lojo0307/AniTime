47ce2db49c1ecf2019158323d32d5a9952485d19
임성원 <dnjsndnjs@gmail.com>
2023-08-08 11:14:58 +0900

    Feat: 미팅페이지 헤더에 로고 추가
    Former-commit-id: 4c35f4aa2a79751c4d6b87b92ed1bf7cec749e3b

--- frontend/src/pages/MeetingPage.jsx
+++ frontend/src/pages/MeetingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d0f3dd54fba7e4ed0eabfaa0eb0285f8e18ff9374119228148061243bf5ab6f
-size 8547
+oid sha256:d23dff2bf930de6a670a3e987d587ef93a9af5ebabaa70338ef185efd470a919
+size 8576
