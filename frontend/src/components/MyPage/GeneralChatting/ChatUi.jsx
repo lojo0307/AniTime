fe96b0fc9db321c18a5be95637f0d0c81de1ac78
권지훈 <aspire51@naver.com>
2023-08-08 16:49:43 +0900

    김민태가 문제였따.
    Former-commit-id: 2b3ab64565cd771a8de0adcef9235b3f6dc5104b

--- frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
+++ frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:708980b7c216affec46918942921595129e6d505e7f0d9a100e0cd6c9d8918bd
-size 6300
+oid sha256:af9536bbcff196d1c68bc2ef15db0c61c8768127b24a010132161e7e7c542f13
+size 6303
