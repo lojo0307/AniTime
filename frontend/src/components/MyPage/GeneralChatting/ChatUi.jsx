9815ea1cd5ba589525e842f0fc332828412a5467
권지훈 <aspire51@naver.com>
2023-08-08 17:31:53 +0900

    Update frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
    Former-commit-id: 3f9745a636dacc8fa5d2d203fa23a6a3ed2b8c1b

--- frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
+++ frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe3a88d4e1c7c1076b30b2ac0636369e2bfe59c64470648bb857edb848ec52d2
-size 6304
+oid sha256:f2590f9c2f76063ebb805cbf450d893f525aaa7429eaa69c61f5011e500608da
+size 6308
