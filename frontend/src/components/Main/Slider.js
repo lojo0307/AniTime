26281c1c98dade0f64ebb47f827808b4f1042325
임성원 <dnjsndnjs@gmail.com>
2023-08-08 13:48:53 +0900

    Feat: 메인페이지 슬라이더 수정
    Former-commit-id: 76bedd022a6461a9c9b4baf4357b048f155725a2

--- frontend/src/components/Main/Slider.js
+++ frontend/src/components/Main/Slider.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2352ec3ae7f2af9d8e8cdb75952580a0b76e405a6a6314672116018782e5935c
-size 2700
+oid sha256:1d05d52ba46be64bd253a8707cae330561ea14c971991c63b0a5671e8b38aa78
+size 2854
--- frontend/src/components/Main/SliderItem.js
+++ frontend/src/components/Main/SliderItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d70dd6a5a733c26d2a6fc87ce07ded3c23d0572ff48b615fbecd1f41daa0ae0
-size 665
+oid sha256:3f531d5686d0dd7f873822de6e68e384b5525daa46937d93de957d53e935caf5
+size 521
