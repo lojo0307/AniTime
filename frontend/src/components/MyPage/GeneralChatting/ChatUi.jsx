0604ec8f3321da68a120e7261a7f503a5287e6df
권지훈 <aspire51@naver.com>
2023-08-08 17:41:31 +0900

    제발 되라 소켓아!!!!!
    Former-commit-id: 14e20352a2273e6285a765c21bdf90d265a9e405

--- frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
+++ frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2590f9c2f76063ebb805cbf450d893f525aaa7429eaa69c61f5011e500608da
+oid sha256:82da788feb75ccb110c465f982f79c5990415dff8f4888c9fae6b7c42abdec48
 size 6308
