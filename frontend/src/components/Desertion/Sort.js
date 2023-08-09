ce29d8f2ab3080e22137ed907e8217d596bb9fce
jaewoong <jwhur7@naver.com>
2023-08-09 15:35:00 +0900

    Feat:정렬 버튼 화살표
    Former-commit-id: 0a460f3efb8f84f8e08b30a46cc846e834afcf8b

--- frontend/src/components/Desertion/Sort.js
+++ frontend/src/components/Desertion/Sort.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:467f1f9ab9d02acbbfebd79c31c82e8cb1092c950361e74c740fd50c17461173
-size 1594
+oid sha256:5369e7158b2261cb218e9ff6e607ae9756ab6a924cb2e23ab77820fc720a7784
+size 1790
