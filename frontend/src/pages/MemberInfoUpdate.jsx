1e8d40c1895e6c26bc0e0fb28a4cd9a519becd0e
권지훈 <aspire51@naver.com>
2023-08-09 15:36:54 +0900

    fix : text fix
    Former-commit-id: 4c584674bdfd0542bee3bc4ed76d3d5b18b91ef6

--- frontend/src/pages/MemberInfoUpdate.jsx
+++ frontend/src/pages/MemberInfoUpdate.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a4440728a89d7a6fed8706cb7a9ff88dc06becbac509f7804b5f006b03a9d12
-size 7620
+oid sha256:505213893f4cd546818314eebb8a6f1fb27e26a2ff5aaf819cb0707ac555d6fd
+size 7608
