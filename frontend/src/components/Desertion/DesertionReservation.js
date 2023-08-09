f2c2ede56521589861b5f2c1f54b584ce4d9ee79
DESKTOP-1JVI7QI\SSAFY <prairial6@naver.com>
2023-08-09 17:35:53 +0900

    Feat: 입양 동물 사진 종횡비 맞추기, 스크롤 효과
    Former-commit-id: c68495bff05be6bb096fa875847381211b534ce2

--- frontend/src/components/Desertion/DesertionReservation.js
+++ frontend/src/components/Desertion/DesertionReservation.js
@@ -1,16 +1,3 @@
-import React from "react";
-import { MainContainer } from "styled/styled";
-import { Outlet } from "react-router";
-
-export default function DesertionReservation() {
-  return (
-    <MainContainer
-      style={{
-        maxWidth: "100vw",
-        backgroundColor: "#F7F8FA",
-      }}
-    >
-      <Outlet />
-    </MainContainer>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:09a7dd8d3a0765bf18d07f12e286ad7604e39d74d33493e9693cec3e9ad66523
+size 331
