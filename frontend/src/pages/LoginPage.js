d6fb96f48ef2051f9785821d538654d2e12b426f
권지훈 <aspire51@naver.com>
2023-08-09 13:00:49 +0900

    Update frontend/src/pages/LoginPage.js
    Former-commit-id: 8acc16f4e975deab20d9d493b046db1c661be52f

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ca8dd0c5c5271e7d1406c94f5c207d43dbddff070a41c12dfa3a38961a47c03
-size 5704
+oid sha256:2c709566971da0288de0536608c41fb42bb0e99bdbb8414d0c7d4634bb428702
+size 5708
