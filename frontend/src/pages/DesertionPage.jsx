c5738427f79c19780ae550a1e1aaed0b9af25a49
임성원 <dnjsndnjs@gmail.com>
2023-08-09 12:18:22 +0900

    Feat: 입양, 실종 페이지 동물 리스트 css 그리드 적용
    Former-commit-id: 750ff76fe791196ffb6c427cfa3886198c1022d2

--- frontend/src/pages/DesertionPage.jsx
+++ frontend/src/pages/DesertionPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0e04010f6133d7087e638a25c133bb181031737f2f4543aa0791a452b503712
-size 4111
+oid sha256:ebc81bf000b4ef5829228e6d6dfcecfe1be00153a17fe4e12121efc8bda21b66
+size 4298
