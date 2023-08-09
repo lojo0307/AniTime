ccaa4d928c1ffdb1492628a96c00a2315e7c3760
임성원 <dnjsndnjs@gmail.com>
2023-08-09 12:52:40 +0900

    Feat: Recommend pageable 적용
    Former-commit-id: 4d6439cfba52eb3d97f5a6db98190d595bccb508

--- backend/src/main/java/com/moi/anitime/model/service/animal/AnimalService.java
+++ backend/src/main/java/com/moi/anitime/model/service/animal/AnimalService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f01c1f95682d662b8a6e394f5925dd76eeed907b876fed4d95bff091b231b748
-size 970
+oid sha256:c0e8b01348caf94bc342ae3bcdcc0b8882a1b2e42b7dd27dc464ce3175a92cd4
+size 985
