c27053c7a268f37e0b5459e367b93da09793ce39
김민태 <kimmintae981019@gmail.com>
2023-08-09 16:26:15 +0900

    Feat: 미팅 시에 채팅방 생성 및 채팅 가능
    Former-commit-id: a56c94f23f0ed7feb048cdb516c50d6427c0e7f7

--- backend/src/main/java/com/moi/anitime/model/service/chat/ChatService.java
+++ backend/src/main/java/com/moi/anitime/model/service/chat/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a5cd06146b9416cc4c26f0a57100cdcb90a1d2e9ef384ebe77d6f13aa0dd38
-size 893
+oid sha256:462dcbe318da416d81deeb96ca223e1913e6fd9f056af5b7a416140758f081d5
+size 1031
