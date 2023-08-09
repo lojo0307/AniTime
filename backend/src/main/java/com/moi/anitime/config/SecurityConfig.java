90890f00449f13b690a7bbfd50a7e492e9de2df4
권지훈 <aspire51@naver.com>
2023-08-09 11:01:35 +0900

    fix : dummy update
    Former-commit-id: 12cc33924c026d497a27753c346bd3243e0aab47

--- backend/src/main/java/com/moi/anitime/config/SecurityConfig.java
+++ backend/src/main/java/com/moi/anitime/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8d32ed5e07141585e2a08f0c10dc2820f0e4dc30214820eade20e4a46bc63e
-size 3096
+oid sha256:e4e09f623e322ab50943435eff979f2a92c11eb27b1396962547ce6e7db5d3bf
+size 3159
