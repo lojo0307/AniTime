bc0380ca59bf3697e5f2bd4c8e5a8f56560c4a75
임성원 <dnjsndnjs@gmail.com>
2023-08-10 10:37:33 +0900

    Feat: 로그인 안한 유저가 접근 못하는 url 수정
    Former-commit-id: e52f76a4dbb592bf4352e4d10f708ec3ea09ea58

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65d0345bdbe969b1d9bc516b20396b07e0ebca9bfd6eef02d95e171ad3241a0d
-size 703
+oid sha256:10a52d46589489de54edc9bfc9dee83717c42a52792b60c8ad2f19a559586de4
+size 868
