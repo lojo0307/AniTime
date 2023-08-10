c7ce2ec88eda99e5779467caba4722eb8cc07cf1
임성원 <dnjsndnjs@gmail.com>
2023-08-10 13:03:59 +0900

    Feat: desertionDetail 동물 사진 새창 띄우기 버튼
    Former-commit-id: 01cf0700364c2aa38e18cbf73fa56aa0bd2d2505

--- frontend/src/components/Desertion/DesertionDetail.jsx
+++ frontend/src/components/Desertion/DesertionDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e580bf5a0a754b0b41698331d954f631434c8a7425ddc96e3354db2ac25b779
-size 7842
+oid sha256:37f1118bbc7b04e79cb895156f6f557eccaadaafe2ec80b2738c1489c55b1174
+size 8324
