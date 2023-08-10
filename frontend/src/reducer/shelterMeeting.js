865f0a9b5c39405ed71fa070eab1f10e5bc02e91
jaewoong <jwhur7@naver.com>
2023-08-10 17:48:47 +0900

    Feat: 비회원 북마크 기능 실행 방지
    Former-commit-id: 3eb2994d64874b556b6513a119c3f2768c8f124d

--- frontend/src/reducer/shelterMeeting.js
+++ frontend/src/reducer/shelterMeeting.js
@@ -1,18 +1,3 @@
-const { createSlice } = require("@reduxjs/toolkit");
-
-const initialState = {
-  meetingNo: -1,
-};
-
-const shelterMeetingSlice = createSlice({
-  name: "shelterMeeting",
-  initialState,
-  reducers: {
-    setMeetingNo(state, action) {
-      state.meetingNo = action.payload;
-    },
-  },
-});
-
-export const { setMeetingNo } = shelterMeetingSlice.actions;
-export default shelterMeetingSlice.reducer;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f46d8191c6944aceada574a7b5d955d27e902e52f30796d7b21023fe91d27545
+size 392
