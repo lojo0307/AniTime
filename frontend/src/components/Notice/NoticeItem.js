167a276d1e6dac7abd8156b3d86933a9142eaa17
임성원 <dnjsndnjs@gmail.com>
2023-08-10 17:07:27 +0900

    Feat: 알림창 알림 개행문자 적용
    Former-commit-id: f4acfecc738700539981e7e26e122ebef28bad2b

--- frontend/src/components/Notice/NoticeItem.js
+++ frontend/src/components/Notice/NoticeItem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65976f64e6571010cc46aa35c4b4dbc68ad69bf061d3c97d5761b26b36e7531d
-size 771
+oid sha256:3897a5bb8f45d291ca84561989db6f606aaccad2e58bab01375d458ea48a2ea8
+size 922
