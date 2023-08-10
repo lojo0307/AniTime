13dcf1441e9c1b53c92560f1034ae95a0e1edf9d
jaewoong <jwhur7@naver.com>
2023-08-10 17:48:47 +0900

    Feat: 비회원 북마크 기능 실행 방지
    Former-commit-id: f924f05391edeadd1e5cdd7b50770f4277bc9787

--- frontend/src/components/Desertion/AnimalItem.jsx
+++ frontend/src/components/Desertion/AnimalItem.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d47140c966113fd73bc8a6e7c256fc94311b4bdaa45bae1789601469f0fb6f7f
-size 2697
+oid sha256:8bfb0251d27b680260dd6c17b29ade3df0f2aeb7d4c615e12dbfed07aabe4d73
+size 3196
