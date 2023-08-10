8076026eedf5ffec2a4cb099bbf75def3629c983
권지훈 <aspire51@naver.com>
2023-08-10 14:54:38 +0900

    update : 마이페이지 보호소 개인정보 수정버튼 비활성화
    Former-commit-id: b13e26d2df99a1665aeadca309fb9856464b2028

--- frontend/src/pages/MissingPage.js
+++ frontend/src/pages/MissingPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdb844d7e11b86aeea1efdc10f1fac58da5d9b601dcd084166ef0dbf7971f731
-size 4024
+oid sha256:02dd981bc6d2cd25c540cbafcf60ce5ccc5832bd89e89b6cfcf616cc7e58dc1d
+size 4057
