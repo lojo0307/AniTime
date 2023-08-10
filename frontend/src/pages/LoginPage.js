65c37f49bbb615b477b5c1b6e2d5d93fc9b42898
김민태 <kimmintae981019@gmail.com>
2023-08-10 12:43:59 +0900

    Update LoginPage.js
    Former-commit-id: 03f499e7b582c5ae0c5ef1f75f921fbd3487693e

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:949e74b8e7e85d9deb2dc553684c50cdd123de79e42159898a9aadfba5c3e58a
-size 10494
+oid sha256:4c405fb2ea1a5e2c25993af8858ea4c5b39b88da78a763906c71e30ba1ea64a2
+size 10498
