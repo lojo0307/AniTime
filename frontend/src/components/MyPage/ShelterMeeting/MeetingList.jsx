7b905fd25b909c6fcde7397f250dd439d4c5137a
임성원 <dnjsndnjs@gmail.com>
2023-08-10 16:16:57 +0900

    Feat: 보호소 미팅 승인, 반려시 목록 다시 불러오기
    Former-commit-id: 4141e9cb9fba60f993d79aa79f7bd2f8b3cddf2e

--- frontend/src/components/MyPage/ShelterMeeting/MeetingList.jsx
+++ frontend/src/components/MyPage/ShelterMeeting/MeetingList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbce55da0c0bd0c38ef9380e308870cfe062ad7b31dc9bca54309bcba0494997
-size 2232
+oid sha256:41f2039ccb151ece41fde67c4ca39553df9eed6c66644512d1e1b8fc36f6a408
+size 2685
