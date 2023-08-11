ce8b1652e6b1f4b445a1a61c072bffec7a9d5187
임성원 <dnjsndnjs@gmail.com>
2023-08-11 10:41:05 +0900

    Refactor: 마이페이지 미팅디테일 안쓰는 import 삭제
    Former-commit-id: 379b177d54ba9a639fcd6cf61a971383ad13c1fb

--- frontend/src/components/MyPage/ShelterMeeting/MeetingList.jsx
+++ frontend/src/components/MyPage/ShelterMeeting/MeetingList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41f2039ccb151ece41fde67c4ca39553df9eed6c66644512d1e1b8fc36f6a408
-size 2685
+oid sha256:b912b04cb6e7d667dd5fb2843aff60a24ad1a5ca7122906ed0ae0560ecf55253
+size 2680
