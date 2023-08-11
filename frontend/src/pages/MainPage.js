9494c278a26d526af8b731bb6209cdc78bf401bb
임성원 <dnjsndnjs@gmail.com>
2023-08-11 17:47:58 +0900

    Feat: 메인페이지 스크롤 스냅 적용
    Former-commit-id: 8fb919b20bd69cd8ee493eb2e4e6854f8834123d

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f48b475d1716648fc2f11242286a2b291d0c85798043905ef89800c8dbd4b4cc
-size 5904
+oid sha256:32f430d7ea184bfc78a35d91fdb7541d2f92730f29c55b70cc17c32a8fc71fd0
+size 6656
