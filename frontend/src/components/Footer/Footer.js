05ccbdc5012614bb269b5b20fdbe6ea8e78fafca
임성원 <dnjsndnjs@gmail.com>
2023-08-11 14:33:34 +0900

    Design: 메인페이지 수정, 헤더 푸터 수정
    Former-commit-id: c048ca85c4b212f41110762bf6e3309721c9f196

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7874bb8a3f5de10be0a6fde8397adfe8b4d01e8fbb44ea221b2cb2796956509e
+oid sha256:39096cc6fd0733fc938515b433645e9441e67462a7e94d2a2a34a427079a2f6a
 size 1246
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3f9d51739ffe813b68b6f6425120d88013bd93ddcb480cee0749470eb0f98fc
+oid sha256:3ab26dab941e774e03582ac6a37ff392f93e1a932bfa50abb633a9a77b62e0fb
 size 1868
