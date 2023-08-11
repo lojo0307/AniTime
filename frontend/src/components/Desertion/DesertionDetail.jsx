d1d8c6577febe8bbde76b59bb1100144bf78dea5
jaewoong <jwhur7@naver.com>
2023-08-11 17:44:30 +0900

    Refactor: 채팅 생성 메시지 일회성 부여 및 채팅창 모달
    Former-commit-id: 3bc27bb5899fde4e6828a8e893b2eaa8d62d8fda

--- frontend/src/components/Desertion/DesertionDetail.jsx
+++ frontend/src/components/Desertion/DesertionDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d85ee08cee6295723a6827163dbae494adf27ded06164c3847007d9897cd385
-size 8663
+oid sha256:37363c0b1b483f2868e6f5e75aada89443e4ad0cb38f980fa89692d878b0c29d
+size 8659
