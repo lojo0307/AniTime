7c277556fc564652f6930ec2170acbef8cd33c29
jaewoong <jwhur7@naver.com>
2023-08-11 10:55:22 +0900

    Feat: 필터 및 정렬 버튼 완료
    Former-commit-id: e1c3020c2b5a443a5afefd2c8cf50a742dd79a68

--- frontend/src/components/Desertion/AnimalItem.jsx
+++ frontend/src/components/Desertion/AnimalItem.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bfb0251d27b680260dd6c17b29ade3df0f2aeb7d4c615e12dbfed07aabe4d73
-size 3196
+oid sha256:20666bcda210cc8df13df73dfd3ebf41f2dd1bd3854e6bd97bc09b1ef9e92003
+size 3161
