38f2f94c82ead597d8d49d9edf5c068a9a34318a
김민태 <kimmintae981019@gmail.com>
2023-08-12 22:19:08 +0900

    Feat: rabbitmq back
    Former-commit-id: 29f00f340b8e60dba9baa5fd9f7ac319c5830550

--- backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
+++ backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5355be85830e9153aa9d09a7c4e5c8f928288fab136a1b5300d9a2eecb9bb63e
-size 1127
+oid sha256:e0d44108f9818d4899e1af135c0f5d2f53b848d0fb53e9a37aa3a4b356d74540
+size 1234
