eb9b211be331e06b26be57448b6ab4f91173e1d5
김민태 <kimmintae981019@gmail.com>
2023-08-13 00:12:03 +0900

    Feat: front RabbitMQ 적용
    Former-commit-id: 120ef154e0f86ed090a656f12c7f0c956f1a2775

--- frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
+++ frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5711497a052429810feebf18ac838a542a5ee87d32db11a73ec206980b9264da
-size 7481
+oid sha256:8abb6434819c49a9da9532438ac3156b7138d26134bb73a0dc962eee0094fa6e
+size 7891
