3cd62ca2a4f12f2225e86ff9d870a3f221a7179e
장명주 <lojo0307>
2023-08-13 23:03:29 +0900

    Fix: css 수정
    Former-commit-id: 0144c8b90c4b3a3bb340c92dcb8db16f9f4f09ba

--- frontend/src/components/MyPage/MyPageMeeting.jsx
+++ frontend/src/components/MyPage/MyPageMeeting.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48534cd436248ae5e4727cf0f23e328d185e78a35f8509283c201eed485deec4
-size 3377
+oid sha256:71f3bd0f89ebb3902b0691f63ff9cc46bfc9795fdf72b9fb4b4391145ec820b8
+size 5221
