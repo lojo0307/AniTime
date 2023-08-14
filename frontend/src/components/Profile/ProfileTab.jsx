4280686e11b8509a72498991fef61f1acbc3259d
임성원 <dnjsndnjs@gmail.com>
2023-08-14 13:44:56 +0900

    Design: 입양, 실종 마진 수정
    Former-commit-id: 26ba32dbd27830831c3268a289329ef534e5a7bd

--- frontend/src/components/Profile/ProfileTab.jsx
+++ frontend/src/components/Profile/ProfileTab.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f5344a5f0b49fc692209d4884ecfcc40df05a5365959d473d16a24520a2fc72
-size 2903
+oid sha256:e5b3f9c1b6163e00730a0c6084578b807c8eafd38c89334d7d8ce0a2acf8e7bb
+size 2991
--- frontend/src/pages/MissingPage.js
+++ frontend/src/pages/MissingPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:720ca616868a8a3abad053e84130c0513145ba31f3c382b0b3d6c90f91497700
-size 3721
+oid sha256:2e727292cc98f9f6d7c494bd0999ed98d989b0294bb8baad796c63809a20bead
+size 3727
