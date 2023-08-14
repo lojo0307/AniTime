2c0369ca87846b6119ed18d9fd4da754cf958504
권지훈 <aspire51@naver.com>
2023-08-14 17:49:04 +0900

    fix : bug hot fix hot hot summer ~
    Former-commit-id: 330f4edc749c9999a4563d810a66077d3effdc2f

--- backend/src/main/java/com/moi/anitime/model/service/member/MemberService.java
+++ backend/src/main/java/com/moi/anitime/model/service/member/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2072cd6c960246bbe53ab884ab764ae76b435e8a7dd22a8c7c1ce8ee8175cb22
-size 1745
+oid sha256:244b69af990cb64266bb7005f2327387844e145aee3b139e4fc00276d6fc36a6
+size 1780
