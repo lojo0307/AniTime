80975b2a13b11cebfbb1872602ae6c83d0668007
임성원 <dnjsndnjs@gmail.com>
2023-08-14 17:03:32 +0900

    Fix: 미팅페이지 빠져나갈때 세션 제대로 닫기 + 자기 자신 큰화면에 안뜨게
    Former-commit-id: dbb25799f180fd26ff7527534fca808a2becc92c

--- frontend/src/pages/MeetingPage.jsx
+++ frontend/src/pages/MeetingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fbba7a61dde0cc5ee6b7ae72d2f278b2e0061f8480a6999f1d154352559ddb2
-size 8874
+oid sha256:6c653f93e0775599435f811a2f5fece6b2b97bd05b18e05d4e61819f9ff65ebc
+size 9662
