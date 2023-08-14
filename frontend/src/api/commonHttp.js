16da7e05b3800d91abbea294a643a85f5599eea4
권지훈 <aspire51@naver.com>
2023-08-14 14:55:37 +0900

    Update backend/src/main/resources/application.properties, frontend/src/api/commonHttp.js, frontend/src/pages/LoginPage.js, frontend/src/components/MyPage/GeneralChatting/ChatUi.jsx
    Former-commit-id: 014d5ac682e35b2f4d17be6ef369737a30fdf697

--- frontend/src/api/commonHttp.js
+++ frontend/src/api/commonHttp.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29ddca18148cb25f73ae34c9d6b6f90e179b90283c428c5835d0cd64a7ad48ce
-size 944
+oid sha256:59a5303cec11cd30dcf63a886ccea77d1ab2b6f7fcb7eb0819ab0b19bff4c072
+size 948
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5bff0ca1040fad7f6eb214217f61b7d609f33d3aba481e917b4bca0a513f6e9
-size 10497
+oid sha256:fa3718999fa23952edddfecaa706825cb9e02513bde9798fdfa891c8adb09c11
+size 10501
