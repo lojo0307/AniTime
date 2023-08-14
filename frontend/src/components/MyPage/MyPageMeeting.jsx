c2c4d72177d3ac075499d107e7495522503c6f80
lojo0307 <lojo0307@naver.com>
2023-08-14 13:05:35 +0900

    Revert "Fix: 자잘자잘 수정"
    This reverts commit 161a4cd715a24f7ca787bc7abfc531aa6a55f1c8 [formerly 2ff159b5c7c124e540af9245e756fa4874a05d48].
    
    
    Former-commit-id: 89eee63a82aaa7a1a21e7c58a55b036596127dc5

--- frontend/src/components/MyPage/MyPageMeeting.jsx
+++ frontend/src/components/MyPage/MyPageMeeting.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d35ea1080c739e6a61f7ac81ee47b88b6ba0efb45daf4b5a5fe4dcc7d81f97ca
-size 5806
+oid sha256:71f3bd0f89ebb3902b0691f63ff9cc46bfc9795fdf72b9fb4b4391145ec820b8
+size 5221
