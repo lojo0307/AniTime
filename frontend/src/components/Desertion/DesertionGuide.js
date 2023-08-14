062d97d50ecfac2b39663dfeacf5ce5c5a9c0627
DESKTOP-1JVI7QI\SSAFY <prairial6@naver.com>
2023-08-14 09:51:56 +0900

    Refactor: 불필요한 console.log 문 삭제
    Former-commit-id: 759e0025fff80b9f91476cc3785fa159d2db4733

--- frontend/src/components/Desertion/DesertionGuide.js
+++ frontend/src/components/Desertion/DesertionGuide.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f528a4a722919b8a1fa6f8b1c6de5a55513fe3f703a2a7f75c2dd448c7f459e
-size 3142
+oid sha256:1f8cd9d912168cbf9fbc39a2d18e75af1444d44b1a50e760c2ad9525cfd97257
+size 3056
