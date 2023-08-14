161a4cd715a24f7ca787bc7abfc531aa6a55f1c8
lojo0307 <lojo0307@naver.com>
2023-08-14 13:05:04 +0900

    Fix: 자잘자잘 수정
    Former-commit-id: 2ff159b5c7c124e540af9245e756fa4874a05d48

--- frontend/src/components/Profile/ProfileTab.jsx
+++ frontend/src/components/Profile/ProfileTab.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75ed69851d2e5acd3ee1a55a685bbdc201df5533c566f83aa3bd1505d8a32f4a
-size 2612
+oid sha256:5f5344a5f0b49fc692209d4884ecfcc40df05a5365959d473d16a24520a2fc72
+size 2903
