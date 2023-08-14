5f0e2477a1f80b9b830f755a65a9acb1df4036a7
lojo0307 <lojo0307@naver.com>
2023-08-14 13:05:04 +0900

    Fix: 자잘자잘 수정
    Former-commit-id: d80afe46d350fa1736b57299e9d318a758eb8fcf

--- frontend/src/components/Profile/ProfileDetail.jsx
+++ frontend/src/components/Profile/ProfileDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68069aa85709e3f9d8fdf390f34ef848e0a833e8a1126e5322f3da99513a953f
-size 7867
+oid sha256:23ba1426f68c758168ae1df99f16a97f15df153f291c689e30fbf3c1fd8ed44d
+size 7954
