49ad9109c51babffc77729cda119bffb3f724aa9
lojo0307 <lojo0307@naver.com>
2023-08-14 13:07:01 +0900

    Fix: 자잘자잘 수정
    Former-commit-id: f4f2cb3e22d8cc67fa1b527ae2b17801409b793f

--- frontend/src/components/MyPage/MyPageChatting.jsx
+++ frontend/src/components/MyPage/MyPageChatting.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab66199814f4bb2d43dc6bf2bbc58842a8bf065f3cf71605346a2a78b4f5844c
-size 554
+oid sha256:0ae54106bc144c2da64f153c7d3aa7ce534b5b81d5a0a38aac1682d2c57d6b5b
+size 614
--- frontend/src/pages/MissingRegistPage.jsx
+++ frontend/src/pages/MissingRegistPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d76f1c4f4b9898560dcc56c7237c3ca90cae3d73084d73fa8eea0d71f1e7a358
-size 14988
+oid sha256:1d8250691723705b5c5c136ae100a7efce0f749b3fb073c7781d170b492ab193
+size 15062
