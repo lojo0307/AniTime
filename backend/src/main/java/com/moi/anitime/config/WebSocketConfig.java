9a5f4d5562c30b173fc7b4cb5f41b37685d78d01
김민태 <kimmintae981019@gmail.com>
2023-08-14 13:44:56 +0900

    Update WebSocketConfig.java
    Former-commit-id: caccef7261242e5ea8296918d128a76455568f27

--- backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
+++ backend/src/main/java/com/moi/anitime/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f844b8820295d7d93d023b8c32e877fbb5c67b5c7117eb209f7a8314aef1f5
-size 1233
+oid sha256:0ec9a6da4a7ad1fda15c648f14e359695472b86212007c5c5f19d9770283a3f4
+size 1483
