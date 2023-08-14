878b53212ccf5da8c164f4ce1f7b80d6bff36e20
권지훈 <aspire51@naver.com>
2023-08-14 14:50:06 +0900

    fix : bug hotfix
    Former-commit-id: 4c3ba12c77a6e95e505afa8cb7b19908029389c0

--- frontend/src/components/Main/DesertionMap.jsx
+++ frontend/src/components/Main/DesertionMap.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f9be046fb8e4221623b430d5774aea997b9777d0cbd9c7fe1242c0e473a714e
-size 1490
+oid sha256:0d00f09441fc20492d16ca2e8b81e02c789a32a07794665554aacfa83cb33aa6
+size 1396
