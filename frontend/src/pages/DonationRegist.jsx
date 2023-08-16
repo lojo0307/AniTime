abb8dce9a354832ce9ef6293734a11c51ddf161a
lojo0307 <lojo0307@naver.com>
2023-08-16 13:25:22 +0900

    Fix: 후원공고 날짜 조건 변경
    Former-commit-id: a54931beeb88639ccb88e1a2bf9c537ac97bb6ae

--- frontend/src/pages/DonationRegist.jsx
+++ frontend/src/pages/DonationRegist.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a22232087cc89578b1ed83e5796aa361c53f72ad151b923b74c4739f6854ce67
-size 13143
+oid sha256:1af12eab9c7e013603d11a6193cf8a084746f48f0b51d4c887e882ed7fc8508a
+size 17344
