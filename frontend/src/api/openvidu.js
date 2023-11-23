21274d8154f49f2ca838b65e391ca5db10326214
장명주 <lojo0307@naver.com>
2023-11-23 17:19:17 +0900

    Update frontend/src/api/openvidu.js
    Former-commit-id: 3ee9c0c5c8e0e35ba9abb3ea234c0b66e385b912

--- frontend/src/api/openvidu.js
+++ frontend/src/api/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a216cf86796b7b49512ecbfab8db1cbd0902d9d4f5ce5c7aeca6b4ee1916cb4c
-size 827
+oid sha256:c2cea4808cca5e93f8e310f965da33d93b60cfecf8e66693141ed8272a139f4b
+size 801
